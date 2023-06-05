46abb503fe48ad1ec8b63608c60d033f7c9125f0
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-06-05 15:44:27 +0300

    Create InitialPortalJS.js

---
+++ InitialPortalJS.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    renderCards()
+})
+function renderCards() {
+    $('#card-wrapper').append(`
+<div class="cardItem">
+        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
+        <p class="cardTitle">إجراء ضد مؤسسة </p>
+        <a class='goToCategoryButton' href=""> تعرّف على المزيد</a>
+    </div>
+    <div class="cardItem">
+        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
+        <p class="cardTitle">إجراء مع المؤسسة</p>
+        <a class='goToCategoryButton' href=""> تعرّف على المزيد</a>
+    </div>
+    <div class="cardItem">
+        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
+        <p class="cardTitle">إجراء طلب تحقيق</p>
+        <a class='goToCategoryButton' href=""> تعرّف على المزيد</a>
+    </div>
+    <div class="cardItem">
+        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
+        <p class="cardTitle">تقديم شكوى</p>
+        <a class='goToCategoryButton' href=""> تعرّف على المزيد</a>
+    </div>
+    `)
+}
