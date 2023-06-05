d0d94acbdf7c49b3bcaed57d9d71de29884caef8
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-06-05 16:43:46 +0300

    Update and rename InitialPortalJS7.js to InitialPortalJS8.js

--- InitialPortalJS8.js
+++ InitialPortalJS8.js
@@ -7,33 +7,33 @@ $(document).ready(function () {
 function renderCards() {
     $('#card-wrapper').append(`
 <div class="cardItem">
-        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
+        <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/legalAffairs.png">
         <p class="cardTitle">الشؤون القانونية</p>
         <a class='goToCategoryButton' href=""> تعرّف على القسم</a>
     </div>
     <div class="cardItem">
-        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
+        <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/cardImg.png">
         <p class="cardTitle">تكنولوجيا المعلومات</p>
         <a class='goToCategoryButton' href=""> تعرّف على القسم</a>
     </div>
     <div class="cardItem">
-        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
+        <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/architecture.png">
         <p class="cardTitle">الهندسة</p>
         <a class='goToCategoryButton' href=""> تعرّف على القسم</a>
     </div>
     <div class="cardItem">
-        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
+        <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/operations.png">
         <p class="cardTitle">العمليات</p>
         <a class='goToCategoryButton' href=""> تعرّف على القسم</a>
     </div>
     <div class="cardItem">
-        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
+        <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/research.png">
         <p class="cardTitle">الأبحاث</p>
         <a class='goToCategoryButton' href=""> تعرّف على القسم</a>
     </div>
     <div class="cardItem">
-        <img src="https://srv-k2five/Designer/Image.ashx?ImID=110253">
-        <p class="cardTitle">الأبحاث</p>
+        <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/maintenance.png">
+        <p class="cardTitle">الصيانة</p>
         <a class='goToCategoryButton' href=""> تعرّف على القسم</a>
     </div>
     `)
