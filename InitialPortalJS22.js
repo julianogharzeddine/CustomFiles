c221b673eab765c1610e55633a2b2116fd8d4b70
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-06-09 16:46:00 +0300

    Update and rename InitialPortalJS21.js to InitialPortalJS22.js

--- InitialPortalJS22.js
+++ InitialPortalJS22.js
@@ -7,31 +7,6 @@ $(document).ready(function () {
         changeLanguage()
     })
 
-     // Translating the Page On Load
-
-  setTimeout(function () {
-    let LSLang = localStorage.getItem('selected_language')
-
-    switch (LSLang) {
-      case 'en-US':
-        $("a.dd-option label.dd-option-text:contains('Arabic')").click();
-        $("a.dd-option label.dd-option-text:contains('English')").click();
-        break
-      case 'ar-SA':
-        $("a.dd-option label.dd-option-text:contains('Arabic')").click();
-        $("a.dd-option label.dd-option-text:contains('Arabic')").click();
-        break
-      case 'fr-FR':
-        $("a.dd-option label.dd-option-text:contains('Arabic')").click();
-        $("a.dd-option label.dd-option-text:contains('Français')").click();
-        break
-      default:
-        $("a.dd-option label.dd-option-text:contains('Arabic')").click();
-    
-        break
-    }
-  }, 2000)
-
 })
 
 
@@ -56,15 +31,17 @@ function renderCards() {
 <div class="cardItem">
         <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/legalAffairs.png" class='titleImage'>
         <p class="cardTitle" id='LegalAffairs'>الشؤون القانونية</p>
+        <img src='https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/employeeIcon.png' class='employeeIcon'>
         <div class='empNoWrap'>
         <p class='empCountLabel' class='peopleInDepartment'>فردًا في القسم</p>
         <p class='empCount'>36</p>
         </div>
-        <a class='goToCategoryButton browseDepartmentDetails' href="https://srv-k2five/Designer/Runtime/Form/LandingPage/" > تعرّف على القسم</a>
+        <a class='goToCategoryButton browseDepartmentDetails' href="https://srv-k2five/Runtime/Runtime/Form/LandingPage/" > تعرّف على القسم</a>
     </div>
     <div class="cardItem">
         <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/cardImg.png" class='titleImage'>
         <p class="cardTitle" id='IT'>تكنولوجيا المعلومات</p>
+        <img src='https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/employeeIcon.png' class='employeeIcon'>
         <div class='empNoWrap'>
         <p class='empCountLabel' class='peopleInDepartment'>فردًا في القسم</p>
         <p class='empCount'>24</p>
@@ -74,6 +51,7 @@ function renderCards() {
     <div class="cardItem">
         <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/architecture.png" class='titleImage'>
         <p class="cardTitle" id='Architecture'>الهندسة</p>
+        <img src='https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/employeeIcon.png' class='employeeIcon'>
         <div class='empNoWrap'>
         <p class='empCountLabel' class='peopleInDepartment'>فردًا في القسم</p>
         <p class='empCount'>130</p>
@@ -83,6 +61,7 @@ function renderCards() {
     <div class="cardItem">
         <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/operations.png" class='titleImage'>
         <p class="cardTitle" id='Operations'>العمليات</p>
+        <img src='https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/employeeIcon.png' class='employeeIcon'>
         <div class='empNoWrap'>
         <p class='empCountLabel' class='peopleInDepartment'>فردًا في القسم</p>
         <p class='empCount'>42</p>
@@ -92,6 +71,7 @@ function renderCards() {
     <div class="cardItem">
         <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/research.png" class='titleImage'>
         <p class="cardTitle" id='Research'>الأبحاث</p>
+        <img src='https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/employeeIcon.png' class='employeeIcon'>
        <div class='empNoWrap'>
         <p class='empCountLabel' class='peopleInDepartment'>فردًا في القسم</p>
         <p class='empCount'>26</p>
@@ -101,6 +81,7 @@ function renderCards() {
     <div class="cardItem">
         <img src="https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/maintenance.png" class='titleImage'>
         <p class="cardTitle" id='Maintenance'>الصيانة</p>
+        <img src='https://cdn.jsdelivr.net/gh/julianogharzeddine/CustomFiles@main/employeeIcon.png' class='employeeIcon'>
         <div class='empNoWrap'>
         <p class='empCountLabel' class='peopleInDepartment'>فردًا في القسم</p>
         <p class='empCount'>67</p>
@@ -133,10 +114,6 @@ function translateToEnglish() {
     $("#Maintenance").text("Maintenance")
     $('#LegalAffairs').text("Legal Affairs")
     $('.cardTitle').css('transform', 'scale(0.8)')
-    $("[name='Sidebar']").css('right' , '')
-    $("[name='Sidebar']").css('left' , '0')
-    $(".form").css('right' , '')
-    $(".form").css('left' , '32%')
 }
 
 function translateToArabic() {
@@ -152,9 +129,5 @@ function translateToArabic() {
     $("#Maintenance").text("الصيانة")
     $('#LegalAffairs').text("الشؤون القانونية")
     $('.cardTitle').css('transform', 'scale(1.05)')
-    $("[name='Sidebar']").css('left' , '')
-    $("[name='Sidebar']").css('right' , '0')
-    $(".form").css('left' , '')
-    $(".form").css('right' , '20%')
 
 }
