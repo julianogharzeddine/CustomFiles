fa6107769ccccf15c03b7a7072afb61b518ac4a2
julianogharzeddine <135016168+julianogharzeddine@users.noreply.github.com>
2023-06-14 14:24:41 +0300

    Update and rename LandingPageJS47.js to LandingPageJS48.js

--- LandingPageJS48.js
+++ LandingPageJS48.js
@@ -147,21 +147,21 @@ $('body').append(`<div class="taskDD">
 </div>
 
 <div id="dropdownContent" style="/* margin-top: 3px; */">
-  <a href="https://srv-k2five/Runtime/Runtime/Form/Review.Form/?SerialNo=39357_29&ServiceID=cc50a549-e868-4a9b-b3ff-f8901c8ec091&Task=%D9%88%D9%83%D9%8A%D9%84%20%D8%A7%D9%84%D9%85%D8%AD%D8%A7%D9%81%D8%B8%20%D9%84%D9%84%D8%B4%D8%A4%D9%88%D9%86%20%D8%A7%D9%84%D9%82%D8%A7%D9%86%D9%88%D9%86%D9%8A%D8%A9%20%D9%88%D8%A7%D9%84%D8%AD%D9%88%D9%83%D9%85%D8%A9" target="_blank">
+  <a href="https://srv-k2five/Runtime/Runtime/Form/Review.Form/?SerialNo=39319_332&ServiceID=9c42e00c-db7b-479a-adda-7f762a815da2&Task=%D9%85%D9%83%D8%AA%D8%A8%20%D8%A7%D9%84%D9%85%D8%AD%D8%A7%D9%81%D8%B8" target="_self">
     <div class="date-icon" style="background-color: #0066cc;">اليوم</div>
     <div class="task-details">
       <h4>وكيل المحافظ للشؤون القانونية والحوكمة</h4>
       <p>IAD - 001071 : رقم الطلب </p>
     </div>
   </a>
-  <a href="task2.html" target="_blank">
+  <a href="task2.html" target="_self">
     <div class="date-icon" style="background-color: #0066cc;">الأربعاء</div>
     <div class="task-details">
       <h4>تشكيل اللجنة</h4>
       <p>IAD - 001073 : رقم الطلب </p>
     </div>
   </a>
-  <a href="task3.html" target="_blank">
+  <a href="task3.html" target="_self">
     <div class="date-icon" style="background-color: #0066cc;">الخميس</div>
     <div class="task-details">
       <h4>وكيل المحافظ للشؤون القانونية والحوكمة</h4>
@@ -404,7 +404,7 @@ function renderTasks() {
     var htmlSnippet = ""
 
     tasks.forEach(function (task) {
-        var taskHtml = '<a href="task' + task.id + '.html" target="_blank">' +
+        var taskHtml = '<a href="task' + task.id + '.html" target="_self">' +
             '<div class="date-icon" style="background-color: ' + task.color + ';">' +
             task.date.ar + '</div>' +
             '<div class="task-details"><h4>' + task.title.ar + '</h4><p>ID: ' +
